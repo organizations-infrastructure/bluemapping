7b5f14e4b6ae80ab0bfffa5488b3d72b8cf0e28b
Romain Cambonie <romain.cambonie@gmail.com>
2023-07-07 10:29:45 +0200

    feat: application repositories visibility to private

--- applications.tf
+++ applications.tf
@@ -10,30 +10,33 @@ locals {
   applications = {
     api = {
       github_repository                    = "api"
+      github_repository_visibility         = "private"
       github_repository_topics             = ["api", "node", "fastify", "typescript", "postgresql", "aws", "ecr", "docker"]
       template_repositories                = ["codingones-github-templates/fastify-api", "codingones-github-templates/aws-application-api"]
       templated_files_variables            = merge(local.common-applications-variables, { __REPOSITORY = "api" })
-      template_fork                        = true
+      template_fork                        = false
       service                              = "ecr"
       policy                               = local.policies.ecr
       allow_force_pushes_to_default_branch = false
     }
     client = {
       github_repository                    = "client"
+      github_repository_visibility         = "private"
       github_repository_topics             = ["client", "node", "typescript", "aws", "cloudfront"]
       template_repositories                = ["codingones-github-templates/angular-client", "codingones-github-templates/aws-application-client"]
       templated_files_variables            = merge(local.common-applications-variables, { __REPOSITORY = "client" })
-      template_fork                        = true
+      template_fork                        = false
       service                              = "s3-client"
       policy                               = local.policies.client
       allow_force_pushes_to_default_branch = false
     },
     compute = {
       github_repository                    = "compute"
+      github_repository_visibility         = "private"
       github_repository_topics             = ["api", "node", "fastify", "typescript", "postgresql", "aws", "ecr", "docker"]
       template_repositories                = ["codingones-github-templates/angular-client", "codingones-github-templates/aws-application-client"]
       templated_files_variables            = merge(local.common-applications-variables, { __REPOSITORY = "compute" })
-      template_fork                        = true
+      template_fork                        = false
       service                              = "s3-client"
       policy                               = local.policies.client
       allow_force_pushes_to_default_branch = false
@@ -51,8 +54,9 @@ module "applications" {
   terraform_organization  = local.project.terraform_cloud_organization
   project                 = local.project.name
 
-  github_repository        = each.value.github_repository
-  github_repository_topics = each.value.github_repository_topics
+  github_repository            = each.value.github_repository
+  github_repository_visibility = each.value.github_repository_visibility
+  github_repository_topics     = each.value.github_repository_topics
 
   template_repositories     = each.value.template_repositories
   templated_files_variables = each.value.templated_files_variables
